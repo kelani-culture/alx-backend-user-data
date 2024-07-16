896a1aa3b9bc9357ca852e64e70b0c2059d92a5f
Null <kelanidarasimi9@gmail.com>
2024-07-16 22:40:43 +0100

    user model created

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,16 @@
+#!/user/bin/env python3
+"""
+User model table
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id: int = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: str = Column(String(250), nullable=True)
+    reset_token: str = Column(String(250), nullable=True)
