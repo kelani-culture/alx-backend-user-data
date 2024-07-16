9167032c15076cf485d15fe1ff450b69dec7e550
Null <kelanidarasimi9@gmail.com>
2024-07-16 22:43:15 +0100

    docstring added to user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,18 +1,22 @@
 #!/user/bin/env python3
 """
-User model table
+User model table created
 """
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
 
 Base = declarative_base()
+
 
 class User(Base):
     """
     User model table object oriented
     """
-    __tablename__ = 'users'
-    id: int = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+
+    __tablename__ = "users"
+    id: int = Column(
+        Integer, nullable=False, autoincrement=True, primary_key=True
+    )
     email: str = Column(String(250), nullable=False)
     hashed_password: str = Column(String(250), nullable=False)
     session_id: str = Column(String(250), nullable=True)
