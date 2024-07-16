5df18bf36d63f5c614f5f2a808273dde9a823062
Null <kelanidarasimi9@gmail.com>
2024-07-16 22:45:29 +0100

    docstring added to user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,10 +14,8 @@ class User(Base):
     """
 
     __tablename__ = "users"
-    id: int = Column(
-        Integer, nullable=False, autoincrement=True, primary_key=True
-    )
-    email: str = Column(String(250), nullable=False)
-    hashed_password: str = Column(String(250), nullable=False)
-    session_id: str = Column(String(250), nullable=True)
-    reset_token: str = Column(String(250), nullable=True)
+    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
