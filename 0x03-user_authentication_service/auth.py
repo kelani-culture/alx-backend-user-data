681e89374a3e6a549b1f07587ec78b41b8e998d0
Null <kelanidarasimi9@gmail.com>
2024-07-17 14:02:49 +0100

    create user hash password and also a register user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
-from bcrypt import  hashpw, gensalt
+from bcrypt import gensalt, hashpw
+from sqlalchemy.orm.exc import NoResultFound
+
 """
 handle user authorization
 """
 
+from db import DB
 
-def _hash_password(password: str)-> bytes:
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str):
+
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists.")
+
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            self._db.add_user(
+                email=email, hashed_password=hashed_password.decode()
+            )
+
+
+def _hash_password(password: str) -> bytes:
     """
     hash user given password
     """
