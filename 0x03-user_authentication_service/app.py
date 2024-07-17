11fc1fbec2ee208b3c8ed93b8d969cd717bfd450
Null <kelanidarasimi9@gmail.com>
2024-07-17 14:24:33 +0100

    simple flask app created  redid

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -7,11 +7,9 @@ from flask import Flask, jsonify
 app = Flask(__file__)
 
 
-@app.route("/", methods=["GET"])
-def message():
-    """simple message"""
-    return jsonify({"message": "Bienvenue"})
-
+@app.route('/', methods=['GET'])
+def welcome():
+    return jsonify(message="Bienvenue")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
